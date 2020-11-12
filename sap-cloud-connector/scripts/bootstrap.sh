d8c3a742d5291e070824cbfd53de10ef88f82380
Patrick Leung <patleung@amazon.com>
2020-11-12 12:40:21 +0000

    updated JVM link

--- sap-cloud-connector/scripts/bootstrap.sh
+++ sap-cloud-connector/scripts/bootstrap.sh
@@ -41,7 +41,7 @@ function downloads_sap_software(){
     
     wget --no-check-certificate --no-cookies --header "Cookie: eula_3_1_agreed=tools.hana.ondemand.com/developer-license-3_1.txt; path=/;" -S https://tools.hana.ondemand.com/additional/sapcc-2.12.5-linux-x64.zip  
     
-    wget --no-check-certificate --no-cookies --header "Cookie: eula_3_1_agreed=tools.hana.ondemand.com/developer-license-3_1.txt; path=/;" -S https://tools.hana.ondemand.com/additional/sapjvm-8.1.065-linux-x64.rpm
+    wget --no-check-certificate --no-cookies --header "Cookie: eula_3_1_agreed=tools.hana.ondemand.com/developer-license-3_1.txt; path=/;" -S https://tools.hana.ondemand.com/additional/sapjvm-8.1.066-macosx-x64.zip
 
 }
 
